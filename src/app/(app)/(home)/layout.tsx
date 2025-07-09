c992fa7ee0a912883d259a54f9e6191ec5364dfd
Steven Lam <stevenl.inbox@gmail.com>
2025-07-09 16:33:32 -0700

    Implemented initial base for search filters, categories, and subcategories

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,18 +1,60 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+
 import React from "react";
 import { Navbar } from "./navbar";
 import { Footer } from "./fotter";
+import { SearchFilters } from "./search-filters"
+import { Category } from '@/payload-types';
 
 interface Props {
   children: React.ReactNode;
 }
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  try {
+      const payload = await getPayload({
+        config: configPromise,
+      })
+  
+      const data = await payload.find({
+        collection: "categories",
+        pagination: false,
+        depth: 1,
+        where: {
+          parent: {
+            exists: false
+          }
+        }
+      })
+
+      const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+          // Because of 'depth: 1' we are confident doc will be a type of category
+          ...(doc as Category),
+          subcategories: undefined,
+        })),
+      }));
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#F4F4F0]">{children}</div>
       <Footer />
     </div>
   );
-};
+
+} catch ( error ) {
+  return (
+    <div>
+      <p>
+        Error loading categories:{" "}
+        {error instanceof Error ? error.message : "Unknown error"}
+      </p>
+    </div>
+  );
+}
+}
 
 export default Layout;
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,34 +1,3 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-
-/**
- * Asynchronously fetches all entries from the "categories" collection and renders them as formatted JSON within a React component.
- *
- * @returns A JSX element displaying the retrieved "categories" data as a formatted JSON string.
- */
-export default async function Home() {
-  
-  try {
-      const payload = await getPayload({
-        config: configPromise,
-      })
-  
-      const data = await payload.find({
-        collection: "categories",
-      })
-  
-      return (
-        <div>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      )
-    } catch (error) {
-      return (
-        <div>
-          <p>Error loading categories: {error instanceof Error ? error.message : 'Unknown error'}</p>
-        </div>
-      )
-    }
+export default function Home() {
+  return <div>Home Page</div>;
 }
-
-
