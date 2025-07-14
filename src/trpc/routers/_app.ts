0f0a7a8d585c424dfd1d00acd6b683fd820cd5af
Steven Lam <stevenl.inbox@gmail.com>
2025-07-14 16:02:44 -0700

    Finished initial authentication with the frontend and backend

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,10 @@
+import { authRouter } from "@/modules/auth/server/procedures";
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedure";
+
 export const appRouter = createTRPCRouter({
-  categories: categoriesRouter
+  auth: authRouter,
+  categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
