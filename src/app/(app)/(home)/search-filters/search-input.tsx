99b0a435867a6fe02067324888b844c8a0fb86a9
Steven Lam <stevenl.inbox@gmail.com>
2025-07-15 15:19:24 -0700

    Implemented initial authstates as well as error handling

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,35 +1,52 @@
-"use client"
+"use client";
 
 import { Input } from "@/components/ui/input";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 import { CategoriesSidebar } from "./categories-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface Props {
-    disabled? : boolean;
+  disabled?: boolean;
 }
 
-export const SearchInput = ({disabled} : Props) => {
-
+export const SearchInput = ({ disabled }: Props) => {
   const [isSidebarOpened, setIsSidebarOpened] = useState(false);
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
 
-    return (
-      <div className="flex items-center gap-2 w-full">
-        <CategoriesSidebar open={isSidebarOpened} onOpenChange={setIsSidebarOpened}/>
-        <div className="relative w-full">
-          <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
-          <Input
-            className="pl-8"
-            placeholder="Search Products"
-            disabled={disabled}
-          />
-        </div>
-        {/* TODO: Add categories view all button*/}
-        <Button variant={"elevated"} className="size-12 shrink-0 flex lg:hidden" onClick={() => setIsSidebarOpened(true)}>
-          <ListFilterIcon />
-        </Button>
-        {/* TODO: Add Library button*/}
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar
+        open={isSidebarOpened}
+        onOpenChange={setIsSidebarOpened}
+      />
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
+        <Input
+          className="pl-8"
+          placeholder="Search Products"
+          disabled={disabled}
+        />
       </div>
-    );
-}
+      <Button
+        variant={"elevated"}
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpened(true)}
+      >
+        <ListFilterIcon />
+      </Button>
+      {/* TODO: Add Library button*/}
+      {session.data?.user && (
+        <Button variant={"elevated"} asChild>
+          <Link href="/library">
+            <BookmarkCheckIcon className="mr-2" />
+          </Link>
+        </Button>
+      )}
+    </div>
+  );
+};
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -13,13 +13,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { registerSchema } from "../../schemas";
+import { registerSchema } from "../../ui/schemas";
 import z from "zod";
 import Link from "next/link";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
@@ -31,12 +31,14 @@ const poppins = Poppins({
 export const SignUpView = () => {
   const router = useRouter();
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
   const register = useMutation(
     trpc.auth.register.mutationOptions({
       onError: (error) => {
         toast.error(error.message);
       },
-      onSuccess: () => {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.auth.session.queryFilter());
         router.push("/");
       },
     })
