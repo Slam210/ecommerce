7498b3084730352a85d084aea3df849b84b18f75
Steven Lam <stevenl.inbox@gmail.com>
2025-07-19 20:08:57 -0700

    Update src/modules/tenants/ui/components/footer.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 export const Footer = () => {
   return (
     <footer className="border-t font-medium bg-white">
-      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full gap-2 px-4 py-6 lg:px-12">
+      <div className="max-w-xl mx-auto flex items-center h-full gap-2 px-4 py-6 lg:px-12">
         <p>Powered By</p>
         <Link href="/">
           <span className={cn("text-2xl font-semibold", poppins.className)}>
