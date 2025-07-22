f823c875a0d60f7358990ce2bed7a1c1f9aa90b0
Steven Lam <stevenl.inbox@gmail.com>
2025-07-22 16:54:01 -0700

    Implmented initial checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,6 +4,7 @@ import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { productsRouter } from "@/modules/products/server/procedure";
 import { tagsRouter } from "@/modules/tags/server/procedure";
 import { tenantsRouter } from "@/modules/tenants/server/procedure";
+import { checkoutRouter } from "@/app/(app)/(tenants)/tenants/[slug]/(checkout)/server/procedure";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   products: productsRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
